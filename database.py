d07d981372259fdd1836fc463eba42eafebbcf99
abdulsami <msami.bese22seecs@student.nust.edu.pk>
2024-03-07 02:03:05 +0500

    jnj

--- database.py
+++ database.py
@@ -1,11 +1,18 @@
-import sqlite3
+import mysql.connector
 
-# Connection to SQLite database
-conn = sqlite3.connect('database.db',check_same_thread=False)
+# Connection to MySQL database
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Sami@1234",
+    database="database",
+)
 
 # Create a cursor object to execute SQL queries
 cursor = conn.cursor()
 
+# Your SQL queries go here
 
-# Close the connection
-
+# Commit the changes and close the connection
+conn.commit()
+conn.close()
---
+++ databaseschema.sql
@@ -0,0 +1,35 @@
+
+--create database first in mysql workbench then run this below sql code , it will create
+--database
+
+
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    hashed_password TEXT NOT NULL,
+    phone_number TEXT,
+    email VARCHAR(255)
+);
+
+CREATE TABLE admins (
+    admin_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    admin_email VARCHAR(255) NOT NULL UNIQUE,
+    hashed_password TEXT NOT NULL
+);
+
+CREATE TABLE articles (
+    article_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    title TEXT NOT NULL,
+    content TEXT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE posts (
+    post_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    title TEXT NOT NULL,
+    content TEXT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
