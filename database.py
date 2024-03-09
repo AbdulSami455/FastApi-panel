7b92d16c6eccb3bb522df81adec2bae9b3c8c588
Muhammad Muaz <muazgill435@gmail.com>
2024-03-09 15:58:44 +0500

    change password

--- database.py
+++ database.py
@@ -3,13 +3,12 @@ import mysql.connector
 # Connection to MySQL database
 conn = mysql.connector.connect(
     host="localhost",
-    user="root",
-    password="Sami@1234",
-    database="database",
+    user="usernew",
+    password="yourpassword",
+    database="database2",
 )
 
 # Create a cursor object to execute SQL queries
 cursor = conn.cursor()
-
 
 conn.commit()
